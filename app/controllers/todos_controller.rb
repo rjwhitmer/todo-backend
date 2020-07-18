12e240ab2b2f13281eccef56cfd077ad579d2454
Bob Whitmer <rjwhitmercc@gmail.com>
2020-07-18 17:25:44 -0600

    add and delete functionality for todo cards

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,20 @@ class TodosController < ApplicationController
         @todo = Todo.all
         render json: @todo
     end
+
+    def create
+        @todo = Todo.create(
+            title: params[:title],
+            content: params[:content],
+            urgent: params[:urgent],
+            done: params[:done]
+        )
+        render json: @todo, status: :created
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        render status: :no_content
+    end
 end
